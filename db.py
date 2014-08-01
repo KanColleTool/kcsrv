b0662b74d60989a35cadff576a411f5065d7afc6
Johannes Ekberg <uppfinnarn@gmail.com>
2014-08-01 22:09:36 +0200

    DB and Security setup

--- db.py
+++ db.py
@@ -1,5 +1,24 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.security import UserMixin, RoleMixin
 
 db = SQLAlchemy()
 
+role__user = db.Table('role__user',
+	db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
+)
 
+class Role(db.Model, RoleMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80), unique=True)
+	description = db.Column(db.Text)
+
+class User(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	active = db.Column(db.Boolean)
+	email = db.Column(db.String(255), unique=True)
+	username = db.Column(db.String(16), unique=True)
+	password = db.Column(db.String(255), nullable=False)
+	confirmed_at = db.Column(db.DateTime())
+	
+	roles = db.relationship('Role', secondary=role__user, backref=db.backref('users', lazy='dynamic'))
--- kcsrv.py
+++ kcsrv.py
@@ -2,16 +2,21 @@
 import os
 from flask import Flask, render_template
 from flask.ext.migrate import Migrate
-from db import db
+from flask.ext.security import Security, SQLAlchemyUserDatastore
+from db import *
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
+app.config['DEBUG'] = None
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(ROOT_PATH, "kcsrv.db")
 app.config.from_object('config')
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
 @app.route('/')
 def index():
