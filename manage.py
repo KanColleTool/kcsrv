b001530004d856462b1edd5c6053e3c4cb0ea466
Johannes Ekberg <uppfinnarn@gmail.com>
2014-08-01 21:34:22 +0200

    Use reloader for runserver

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Server
 from flask.ext.migrate import MigrateCommand
 from kcsrv import app
 
 manager = Manager(app)
+manager.add_command('runserver', Server(host='0.0.0.0', use_reloader=True))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
