f386f5810a7d980945420b001492d5101a8b350d
Johannes Ekberg <uppfinnarn@gmail.com>
2014-08-03 22:13:27 +0200

    I... don't really need all those includes here

--- forms.py
+++ forms.py
@@ -1,10 +1,6 @@
 # coding=utf-8
 from flask.ext.wtf import Form
-from flask.ext.security import current_user
-from flask.ext.security.utils import verify_password
-from wtforms_alchemy import model_form_factory, ModelFieldList
-from wtforms import fields, validators, ValidationError
-from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms_alchemy import model_form_factory
 from db import *
 
 BaseModelForm = model_form_factory(Form)
