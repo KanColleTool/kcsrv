ab1c469e32dca89a4f2b8e5da1f2ebe80dc0da7b
Johannes Ekberg <uppfinnarn@gmail.com>
2014-08-04 01:49:13 +0200

    Added a setup command that installs default roles

--- manage.py
+++ manage.py
@@ -2,10 +2,18 @@
 from flask.ext.script import Manager, Server
 from flask.ext.migrate import MigrateCommand
 from kcsrv import app
+from db import *
 
 manager = Manager(app)
 manager.add_command('runserver', Server(host='0.0.0.0', use_reloader=True))
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def setup():
+	print "Installing default roles..."
+	db.session.add(Role(name="admin", description="Allowed to access the admin panel"))
+	db.session.add(Role(name="staff", description="Allowed to see restricted information"))
+	db.session.commit()
 
 if __name__ == '__main__':
 	manager.run()
