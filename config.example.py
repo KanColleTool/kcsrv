b94ecae69efd9761b5c66227655eb8d409ee4ac5
Johannes Ekberg <uppfinnarn@gmail.com>
2014-08-04 03:14:54 +0200

    Added Sendfile note for vboxsf
    Also SECRET_KEY should be commented by default

--- config.example.py
+++ config.example.py
@@ -5,4 +5,8 @@
 
 
 # Fill in some long, random and utterly unguessable string here
-SECRET_KEY = ''
+#SECRET_KEY = ''
+
+# Use this on when working from a VirtualBox share, needed due to a VBoxSF bug
+# https://www.virtualbox.org/ticket/9069
+#USE_X_SENDFILE = False
