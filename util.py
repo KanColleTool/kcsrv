a9420dff30cbad0e874c8ec1f622a1a796a0c45a
Johannes Ekberg <uppfinnarn@gmail.com>
2014-08-06 03:24:51 +0200

    Helpers for dealing with game data

--- util.py
+++ util.py
@@ -1,5 +1,26 @@
+import os
 import string
 import random
+import json
+
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def generate_api_token():
 	return ''.join(random.choice(string.hexdigits) for _ in range(40)).lower()
+
+def svdata(obj, code=1, message="成功"):
+	res = {
+		"api_result": code,
+		"api_result_msg": message,
+		"api_data": obj
+	}
+	return (
+		"svdata=" + json.dumps(res, separators=(',', ':')),
+		200,
+		{
+			"Content-Type": "text/plain"
+		})
+
+def load_datadump(filename):
+	with open(os.path.join(ROOT_DIR, 'data', filename)) as f:
+		return json.load(f)
