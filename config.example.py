75f54c13a5d8188a9fcc4f6dd7f0ebe66245dc5e
SunDwarf <eyesismine@gmail.com>
2015-05-04 12:42:37 +0100

    Re-add config example, and add py-bcrypt

---
+++ config.example.py
@@ -0,0 +1,9 @@
+#
+# Copy this file to 'config.py' and then edit that
+#
+
+# Fill in some long, random and utterly unguessable string here
+#SECRET_KEY = ''
+
+# Put your password salt here
+# SECURITY_PASSWORD_SALT = ""
