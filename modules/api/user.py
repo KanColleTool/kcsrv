e9c4d47d4be58b18fd3b95c0b4104f11fa0c0931
SunDwarf <eyesismine@gmail.com>
2015-05-04 21:06:10 +0100

    Reformat some things, and add some better routes

--- modules/api/user.py
+++ modules/api/user.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, g
+from flask import Blueprint
 # from flask.ext.security import current_user
 from util import *
-from . import placeholderdata
+from modules import placeholderdata
+import generate_port
 
 api_user = Blueprint('api_user', __name__)
 prepare_api_blueprint(api_user)
@@ -32,8 +33,8 @@ def basic():
         'api_max_chara': admiral.max_ships,
         'api_max_slotitem': admiral.max_equips,
         'api_max_kagu': admiral.max_furniture,
-        'api_playtime': 0,
-        'api_tutorial': "0",  # Regardless, this just gets fucking ignored.
+        'api_playtime': 1,
+        'api_tutorial': "0",
         'api_furniture': admiral.furniture.split(','),
         'api_count_deck': admiral.available_fleets,
         'api_count_kdock': admiral.available_cdocks,
@@ -47,8 +48,8 @@ def basic():
         'api_pt_lose': admiral.pvp_total - admiral.pvp_successes,  # These two aren't even in the game...
         'api_pt_challenged': 0,
         'api_pt_challenged_win': 0,
-        'api_firstflag': 0,
-        'api_tutorial_progress': 100,  # Again, ignored.
+        'api_firstflag': 1,  # Disables the opening stuff, and skips straight to the game.
+        'api_tutorial_progress': 100,
         'api_pvp': [0, 0]
     })
 
@@ -58,19 +59,19 @@ def furniture():
     # TODO: Implement this properly
     admiral = get_token_admiral_or_error()
     return svdata([{
-                       'api_member_id': admiral.id,
-                       'api_id': item.id,
-                       'api_furniture_type': item.type,
-                       'api_furniture_no': item.no,
-                       'api_furniture_id': item.id
-                   } for item in []])
+        'api_member_id': admiral.id,
+        'api_id': item.id,
+        'api_furniture_type': item.type,
+        'api_furniture_no': item.no,
+        'api_furniture_id': item.id
+    } for item in []])
 
 
 @api_user.route('/api_get_member/slot_item', methods=['GET', 'POST'])
 def slot_item():
     # TODO: Implement this properly
     admiral = get_token_admiral_or_error()
-    return svdata([{'api_id': item.id, 'api_slotitem_id': item.itemid, 'api_locked': 0 } for item in []])
+    return svdata([{'api_id': item.id, 'api_slotitem_id': item.itemid, 'api_locked': 0} for item in []])
 
 
 @api_user.route('/api_get_member/useitem', methods=['GET', 'POST'])
@@ -78,16 +79,17 @@ def useitem():
     # TODO: Implement this properly
     admiral = get_token_admiral_or_error()
     return svdata([{
-                       'api_member_id': admiral.id,
-                       'api_id': item.id,
-                       'api_value': item.count,
-                       'api_usetype': item.type,
-                       'api_category': item.category,
-                       'api_name': item.name,  # WHY
-                       'api_description': ["", ""],
-                       'api_price': 0,
-                       'api_count': item.count
-                   } for item in []])
+        'api_member_id': admiral.id,
+        'api_id': item.id,
+        'api_value': item.count,
+        'api_usetype': item.type,
+        'api_category': item.category,
+        'api_name': item.name,  # WHY
+        'api_description': ["", ""],
+        'api_price': 0,
+        'api_count': item.count
+    } for item in []])
+
 
 
 @api_user.route('/api_get_member/kdock', methods=['GET', 'POST'])
@@ -95,39 +97,39 @@ def kdock():
     # TODO: Implement this properly
     admiral = get_token_admiral_or_error()
     return svdata([{
-                       'api_member_id': admiral.id,
-                       'api_id': dock.id,
-                       'api_state': dock.state,
-                       'api_created_ship_id': dock.ship,
-                       'api_complete_time': dock.complete,  # TODO: Convert this to JST
-                       'api_complete_time_str': dock.complete.strftime('%Y-%M-%d %H:%M:%S'),
-                       'api_item1': dock.fuel,
-                       'api_item2': dock.ammo,
-                       'api_item3': dock.steel,
-                       'api_item4': dock.baux,
-                       'api_item5': dock.cmats
-                   } for dock in []])
+        'api_member_id': admiral.id,
+        'api_id': dock.id,
+        'api_state': dock.state,
+        'api_created_ship_id': dock.ship,
+        'api_complete_time': dock.complete,  # TODO: Convert this to JST
+        'api_complete_time_str': dock.complete.strftime('%Y-%M-%d %H:%M:%S'),
+        'api_item1': dock.fuel,
+        'api_item2': dock.ammo,
+        'api_item3': dock.steel,
+        'api_item4': dock.baux,
+        'api_item5': dock.cmats
+    } for dock in []])
 
 
 @api_user.route('/api_get_member/unsetslot', methods=['GET', 'POST'])
 def unsetslot():
     # TODO: Figure out what the hell this even is!
-    return svdata({})
+    return svdata(placeholderdata.unsetslot)
+
 
 @api_user.route('/api_port/port', methods=['GET', 'POST'])
 def port():
-    # This does fuck-all.
-    for val in request.values:
-        print(val, request.values.get(val))
-    return svdata({})
+    api_token = request.values.get('api_token', None)
+    if api_token is None:
+        abort(403)
+    print(generate_port.generate_port(api_token))
+    return svdata(generate_port.generate_port(api_token))
 
 
 @api_user.route('/api_req_init/firstship', methods=['GET', 'POST'])
 # Kancolle literally doesn't care, as long as it gets something back
 def firstship():
     admiral = get_token_admiral_or_error()
-    for val in request.values:
-        print(val, request.values.get(val))
     return svdata({'api_result_msg': 'shitty api is shitty', 'api_result': 1})
 
 
@@ -144,7 +146,4 @@ def misc(path):
 
 @api_user.route('/api_get_member/<path:path>', methods=['GET', 'POST'])
 def misc2(path):
-    d = {}
-    for val in request.values:
-        d[val] = request.values[val]
     return svdata({'api_result_msg': '申し訳ありませんがブラウザを再起動し再ログインしてください。', 'api_result': 201})
