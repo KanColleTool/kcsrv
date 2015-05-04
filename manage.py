5286e278aae33f48ade829a8fb226b361fc4b948
SunDwarf <eyesismine@gmail.com>
2015-05-04 21:04:50 +0100

    Reformat manage.py

--- manage.py
+++ manage.py
@@ -9,14 +9,17 @@ manager.add_command('runserver', Server(host='0.0.0.0', use_reloader=True))
 manager.add_command('db', MigrateCommand)
 
 from commands.user import manager as user_manager
+
 manager.add_command('user', user_manager)
+
 
 @manager.command
 def setup():
-	print("Installing default roles...")
-	db.session.add(Role(name="admin", description="Allowed to access the admin panel"))
-	db.session.add(Role(name="staff", description="Allowed to see restricted information"))
-	db.session.commit()
+    print("Installing default roles...")
+    db.session.add(Role(name="admin", description="Allowed to access the admin panel"))
+    db.session.add(Role(name="staff", description="Allowed to see restricted information"))
+    db.session.commit()
+
 
 if __name__ == '__main__':
-	manager.run()
+    manager.run()
