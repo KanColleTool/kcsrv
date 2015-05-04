f261b6c045f8a6fe8c689f34bd1575ce5fa43a34
SunDwarf <eyesismine@gmail.com>
2015-05-04 21:13:51 +0100

    Add DB update check to kcsrv

--- kcsrv.py
+++ kcsrv.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, send_from_directory
+import os
+import hashlib
+from flask import Flask, render_template, send_from_directory, redirect
 from flask.ext.migrate import Migrate
-from flask.ext.security import Security, SQLAlchemyUserDatastore
+from flask.ext.security import Security, SQLAlchemyUserDatastore, current_user
 from forms import *
 from admin import admin
 
@@ -34,13 +36,55 @@ app.register_blueprint(api_user, url_prefix='/kcsapi')
 
 from modules.resources import resources
 
-app.register_blueprint(resources, url_prefix='/resources')
+app.register_blueprint(resources, url_prefix='/kcs/resources')
 
 # --> Base application routes
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/kcs/<path:path>')
+def kcs(path):
+    return send_from_directory('kcs', path)
+
+@app.route('/kcs/sound/titlecall/a/<path:path>')
+def sound(path):
+    return send_from_directory('kcs/titlecall/a', path)
+
+@app.route('/kcs/sound/<path:path>')
+def shipsound(path):
+    return send_from_directory('kcs/sound', path)
+
+@app.route('/update_db')
+def update_db():
+    curr = current_user
+    if hasattr(curr, "roles"):
+        if 'admin' in curr.roles:
+            x = util.update_db()
+            return "Updated {} entries".format(str(x)), 200
+        else:
+            return "Unauthorized", 403
+    else:
+        return redirect('/account/login')
 
 if __name__ == '__main__':
+    print("Checking for updated api_start2.json...")
+    if not os.path.exists("data/api_start2.json.sha256"):
+        update = True
+        # Generate SHA256 hash
+        h = hashlib.sha256((open('data/api_start2.json').read().encode()))
+        with open('data/api_start2.json.sha256', 'w') as f:
+            f.write(h.hexdigest())
+        print("api_start2 hash file not found - updating database.")
+    else:
+        h = hashlib.sha256(open('data/api_start2.json').read().encode()).hexdigest()
+        h2 = open('data/api_start2.json.sha256').read().replace('\n', '')
+        if h2 == h:
+            print("DB entries are up to date.")
+            update = False
+        else:
+            print("DB entries differ from api_start2.json. Go to admin/update to update.")
+            update = True
+
     app.run(host='0.0.0.0', debug=True)
+
