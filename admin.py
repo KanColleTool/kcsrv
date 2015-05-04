643b89476232448e322134559c326c26812b566b
SunDwarf <eyesismine@gmail.com>
2015-05-04 21:04:16 +0100

    Reformat file

--- admin.py
+++ admin.py
@@ -4,31 +4,35 @@ from flask.ext.security import current_user
 from wtforms import fields
 from db import *
 
+
 class AdminAuthMixin(object):
-	def is_accessible(self):
-		return current_user.has_role('admin')
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
 
 class MyAdminIndexView(AdminAuthMixin, AdminIndexView):
-	pass
+    pass
+
 
 class MyBaseView(AdminAuthMixin, BaseView):
-	pass
+    pass
+
 
 class MyModelView(AdminAuthMixin, ModelView):
-	pass
-
+    pass
 
 
 class RoleModelView(MyModelView):
-	form_overrides = { 'description': fields.TextAreaField }
+    form_overrides = {'description': fields.TextAreaField}
+
 
 class UserModelView(MyModelView):
-	column_exclude_list = ['password']
-	form_excluded_columns = ['password']
+    column_exclude_list = ['password']
+    form_excluded_columns = ['password']
+
 
 class AdmiralModelView(MyModelView):
-	pass
-
+    pass
 
 
 admin = Admin(index_view=MyAdminIndexView())
