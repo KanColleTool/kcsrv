969adcfc9a510385edfaa7bc42f7f432c87ce4b2
SunDwarf <eyesismine@gmail.com>
2015-05-20 17:22:53 +0100

    Add docs for AdmiralHelper

--- helpers/AdmiralHelper.py
+++ helpers/AdmiralHelper.py
@@ -2,7 +2,46 @@ import util
 import db
 from helpers import ShipHelper
 
+def setup(first_ship_id: int, admiral: db.Admiral):
+    """
+    Sets up an admiral.
+    This is for both APIv1 and APIv2.
+    :param first_ship_id: The ID of the very first ship.
+    :param admiral: The admiral object to setup.
+    :return: The setup admiral.
+    """
+    if admiral.setup: return
+    # Create a new ship.
+    ship = ShipHelper.generate_new_ship(first_ship_id, 0)
+    # Assign ship the correct local ship number.
+    ship.local_ship_num = len(admiral.admiral_ships.all())
+    # Create a new fleet.
+    fleet = db.Fleet()
+    # Add the ship to the first fleet.
+    fleet.ships.append(ship)
+    # Add the ship to the admiral
+    admiral.admiral_ships.append(ship)
+    # Add the fleet to the admiral
+    admiral.fleets.append(fleet)
+    # Give the admiral starting resources
+    admiral.resources = "500,500,500,500,1,1,3,0"
+    # Give the admiral some docks.
+    docks = [db.Dock() for _ in range(8)]
+    admiral.docks = docks
+    # Return the admiral
+    admiral.setup = True
+    return admiral
+
+"""These are API version 1 functions only."""
+
+def get_admiral_furniture():
+    return [int(x) for x in util.get_token_admiral_or_error().furniture.split(',')]
+
 def get_admiral_basic_info():
+    """
+    Gets the basic info for the admiral.
+    :return: A dict containing the KanColle info for the admiral.
+    """
     admiral = util.get_token_admiral_or_error()
     return {
         'api_member_id': admiral.id,
@@ -40,26 +79,3 @@ def get_admiral_basic_info():
         'api_pvp': [0, 0]
     }
 
-
-def setup(first_ship_id: int, admiral: db.Admiral):
-    if admiral.setup: return
-    # Create a new ship.
-    ship = ShipHelper.generate_new_ship(first_ship_id, 0)
-    # Assign ship the correct local ship number.
-    ship.local_ship_num = len(admiral.admiral_ships.all())
-    # Create a new fleet.
-    fleet = db.Fleet()
-    # Add the ship to the first fleet.
-    fleet.ships.append(ship)
-    # Add the ship to the admiral
-    admiral.admiral_ships.append(ship)
-    # Add the fleet to the admiral
-    admiral.fleets.append(fleet)
-    # Give the admiral starting resources
-    admiral.resources = "500,500,500,500,1,1,3,0"
-    # Give the admiral some docks.
-    docks = [db.Dock() for _ in range(8)]
-    admiral.docks = docks
-    # Return the admiral
-    admiral.setup = True
-    return admiral
