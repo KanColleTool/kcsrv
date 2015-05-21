0291030d57ceec8ddcfcc9badab6f76f0746a23a
SunDwarf <eyesismine@gmail.com>
2015-05-21 16:00:39 +0100

    Require copying of config file before running.

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import os
 
 from flask import Flask
 
 app = Flask(__name__)
+
+if not os.path.exists('./config.py'):
+    print("Config file does not exist. You must copy config.example.py to config.py and change the values.")
+    exit(1)
+
 app.config['DEBUG'] = None
 app.config.from_object('config_default')
 app.config.from_object('config')
