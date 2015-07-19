2a3fc164aaabb05d4757fc70cc2a6af832778d24
SunDwarf <eyesismine@gmail.com>
2015-07-19 14:44:40 +0100

    Add new class for initing the server after launch, and remove it from kcsrv.

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import os
 
 from flask import Flask
 
+import init
+
 app = Flask(__name__)
 
 if not os.path.exists('./config.py'):
     print("Config file does not exist. You must copy config.example.py to config.py and change the values.")
     exit(1)
 
-app.config['DEBUG'] = None
 app.config.from_object('config_default')
 app.config.from_object('config')
+
+init.init(app)
---
+++ init.py
@@ -0,0 +1,58 @@
+from flask import render_template, send_from_directory
+from flask.ext.migrate import Migrate
+from flask.ext.security import Security, SQLAlchemyUserDatastore
+
+from forms import *
+from admin import admin
+
+modules = {
+    "migrate": None,
+    "security": None,
+    "user_datastore": None
+}
+
+
+def init(app):
+    # --> Extension setup
+    db.init_app(app)
+    admin.init_app(app)
+
+    modules["migrate"] = Migrate(app, db)
+
+    modules["user_datastore"] = SQLAlchemyUserDatastore(db, User, Role)
+    security = Security(app, modules["user_datastore"], confirm_register_form=MyRegisterForm)
+
+    # --> Register blueprints
+    from modules.play.play import play
+
+    app.register_blueprint(play, url_prefix='/play')
+
+    from modules.api.core import api_core
+
+    app.register_blueprint(api_core, url_prefix='/kcsapi')
+
+    # Declare API v1 blueprints.
+    from modules.api.v1.user import api_user
+    from modules.api.v1.actions import api_actions
+    app.register_blueprint(api_user, url_prefix='/kcsapi')
+    app.register_blueprint(api_actions, url_prefix='/kcsapi')
+
+
+    # Declare API v2 blueprints.
+    from modules.api.v2.AdmiralAPI import AdmiralAPIv2
+    from modules.api.v2.DockAPI import DockAPIv2
+    app.register_blueprint(AdmiralAPIv2, url_prefix='/api/v2/admiral')
+    app.register_blueprint(DockAPIv2, url_prefix='/api/v2/docks')
+
+    from modules.resources import resources
+
+    app.register_blueprint(resources, url_prefix='/kcs')
+
+    # --> Base application routes
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/kcs/<path:path>')
+    def kcs(path):
+        return send_from_directory('kcs', path)
