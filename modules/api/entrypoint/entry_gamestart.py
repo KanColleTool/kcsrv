bb30bf1197f9ee1360a1551a07e7b440aeb6208e
SunDwarf <eyesismine@gmail.com>
2015-10-25 11:18:09 +0000

    Reformat some things, fix an error. Pre-merge commit.

--- modules/api/entrypoint/entry_gamestart.py
+++ modules/api/entrypoint/entry_gamestart.py
@@ -1,7 +1,9 @@
-from util import svdata
 from flask import request,g
+
+from util import svdata
 from helpers import gamestart
 from . import api_game
+
 
 @api_game.route('/api_req_init/firstship', methods=['GET', 'POST'])
 # Kancolle literally doesn't care, as long as it gets something back
@@ -9,6 +11,7 @@ def firstship():
     shipid = request.values.get("api_ship_id")
     g.admiral.add_kanmusu(ship_api_id=shipid, fleet_number=1, position=0)
     return svdata({'api_result_msg': 'shitty api is shitty', 'api_result': 1})
+
 
 @api_game.route('/api_req_member/get_incentive', methods=['GET', 'POST'])
 def get_incentive():
@@ -28,7 +31,7 @@ def furniture():
     """Available furniture."""
     # TODO: Implement this properly
     return svdata([{
-        'api_member_id': admiral.id, 'api_id': item.id, 'api_furniture_type': item.type, 'api_furniture_no': item.no, 'api_furniture_id': item.id
+        'api_member_id': g.admiral.id, 'api_id': item.id, 'api_furniture_type': item.type, 'api_furniture_no': item.no, 'api_furniture_id': item.id
     } for item in []])
 
 
@@ -47,6 +50,7 @@ def useitem():
 def kdock():
     """Krafting docks."""
     return svdata(gamestart.kdock())
+
 
 @api_game.route('/api_get_member/unsetslot', methods=['GET', 'POST'])
 def unsetslot():
